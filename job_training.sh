7dd494f22679d0f7db72b76110cac5d54646c262
Emad Ghalenoei <51389409+emadghalenoei@users.noreply.github.com>
2025-11-10 17:38:39 -0700

    Delete job_training.sh

--- job_training.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=geo
-#SBATCH --time=5-00:00:00
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=20
-#SBATCH --mem=60G
-module load mpich/3.2.1-gnu
-mpirun -np 80 python3 training_dataset.py
